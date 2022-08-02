e849197ed903db33702ed8c61168aa086d923d71
Marvin <marvinmokua@gmail.com>
2022-08-03 00:46:20 +0300

    initial commit

---
+++ svelte/README.md
@@ -0,0 +1,42 @@
+# You have to suffer throught this as well
+
+## Skills
+
+### Backend/Native
+
+- go
+- rust
+- c/c++
+- python
+- nodejs
+- java
+- haskell
+- BF
+
+### Web
+
+- HTML5, CSS3
+- JS/TS
+- SCSS
+- Tailwind CSS
+- React/Next
+- Vue2/Vue3
+- Svelte3/kit
+- D3
+- ThreeJS
+
+### Mobile
+
+- Native Android
+- Flutter
+- React Native
+- NativeScript
+
+### DBMS
+
+- MySQL
+- MariaDB
+- MongoDB
+- Firebase
+
+## Projects
--- svelte/svelte.config.js
+++ svelte/svelte.config.js
@@ -1,33 +1,23 @@
-import vercel from '@sveltejs/adapter-vercel';
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
+import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: [
 		preprocess({
-			scss: {
-				prependData: '@use "src/variables.scss" as *;'
-			}
-		})
+			postcss: true
+		}),
+		mdsvex(mdsvexConfig)
 	],
 
 	kit: {
-		adapter: vercel(),
-
-		// hydrate the <div id="svelte"> element in src/app.html
-		//target: '#svelte',
-
-		vite: {
-			css: {
-				preprocessorOptions: {
-					scss: {
-						additionalData: '@use "src/variables.scss" as *;'
-					}
-				}
-			}
-		}
+		adapter: adapter()
 	}
 };
 
